ff94c7a044f4d5489d96a22286c8d4a22dee19f3
Elena Schulze <e.schulze@synedra.com>
2026-06-19 10:53:28 +0200

    First Design for Pokemon list

--- src/main/java/com/schulze/elena/pokedex/web/PokemonController.java
+++ src/main/java/com/schulze/elena/pokedex/web/PokemonController.java
@@ -1,6 +1,7 @@
 package com.schulze.elena.pokedex.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/pokemon")
 public class PokemonController {
 
-	@GetMapping
-	public String listPokemon() {
+	@GetMapping({"", "/"})
+	public String listPokemon(Model model) {
+		model.addAttribute("pokedex", "700");
+		model.addAttribute("pokemon", "Feelinara");
+		model.addAttribute("type", "Fee");
+		model.addAttribute("strongAgainst", "Drache, Kampf, Unlicht");
+		model.addAttribute("vulnerableTo", "Stahl, Gift");
 		return "pokemon/list.xhtml";
 	}
 
